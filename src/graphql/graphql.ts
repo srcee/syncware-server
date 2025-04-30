41fead7a8ba2548e7522aee1e9f6ae82f6985633
ibrezoev <srce@mail.bg>
2025-04-30 18:43:52 +0300

    feat: added graphql

---
+++ src/graphql/graphql.ts
@@ -0,0 +1,20 @@
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export enum UserRole {
+  SuperAdmin = 'SuperAdmin',
+  Admin = 'Admin',
+}
+
+export interface User {
+  role: UserRole;
+  username: string;
+}
+
+type Nullable<T> = T | null;
